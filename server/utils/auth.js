754a8e22bb8f7159e11b44b72fdabd8309388220
Joseph Flatley <josephflatley@Josephs-MacBook-Pro.local>
2023-07-10 16:14:41 -0500

    mutations

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,27 +1,77 @@
 const jwt = require('jsonwebtoken');
-const { AuthenticationError } = require('apollo-server-express');
-const { secret, expiration } = require('../config/auth'); 
 
-const authMiddleware = (context) => {
-  const { req } = context;
-  let token = req.headers.authorization;
+// set token secret and expiration date
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
 
-  if (req.headers.authorization) {
-    token = token.split(' ').pop().trim();
-  }
+module.exports = {
+  // function for our authenticated routes
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via  req.query or headers
+    let token = req.query.token || req.headers.authorization || req.body.token;
 
-  if (!token) {
-    throw new AuthenticationError('You have no token!');
-  }
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
 
-  try {
-    const { data } = jwt.verify(token, secret, { maxAge: expiration });
-    context.user = data;
-  } catch (err) {
-    console.log('Invalid token');
-    throw new AuthenticationError('Invalid token!');
-  }
+    if (!token) {
+      return req;
+    }
+
+    // verify token and get user data out of it
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+      return res.status(400).json({ message: 'invalid token!' });
+    }
+
+    // send to next endpoint
+    return req;
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
 };
 
-module.exports = authMiddleware;
+// trial 2 
 
+// const jwt = require('jsonwebtoken');
+
+// // set token secret and expiration date
+// const secret = 'mysecretsshhhhh';
+// const expiration = '2h';
+
+// // Apollo Server Auth
+
+// module.exports = {
+//   signToken: function({ username, email, _id }) {
+//     const payload = { username, email, _id };
+
+//     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+//   },
+//   authMiddleware: function({ req }) {
+//     let token = req.body.token || req.query.token || req.headers.authorization;
+//     if (req.headers.authorization) {
+//         token = token
+//             .split(' ')
+//             .pop()
+//             .trim();
+//     }
+//     if (!token) {
+//         return req;
+//     }
+//     try {
+//         const { data } = jwt.verify(token, secret, { maxAge: expiration });
+//         req.user = data;
+//     } catch {
+//         console.log('Invalid token');
+//     }
+//     // return the updated request object
+//     return req;
+//   }
+// };
